c86de93c5067b983f05437b6c1a7224200d9925c
darookee <info@darookee.net>
2012-06-15 12:26:27 +0200

    added tmux/tmx and updated setup script to do backups for existing files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,40 @@
 #!/bin/sh
 git submodule update --init --recursive
 
-for rcfile in `ls -1 .`; do
-    ln -s -f ~/.dotfiles/$rcfile ~/.$rcfile
+# Thanks to Kechol (https://gist.github.com/Kechol) for this gist (https://gist.github.com/1862504)
+
+#symlinks
+PWD=`pwd`
+PREFIX='.'
+DOTFILES=`ls -1`
+IGNOREFILES=( .. bak setup.sh README.md .git )
+
+for DOTFILE in ${DOTFILES[@]}
+do
+    for IGNOREFILE in ${IGNOREFILES[@]}
+    do
+        if [ ${DOTFILE} == ${IGNOREFILE} ]
+        then
+            continue 2
+        fi
+    done
+
+    SYMLINK="${HOME}/${PREFIX}${DOTFILE}"
+    BACKUPTIME=`date +%s`
+    BACKUPDIR="${PWD}/${PREFIX}dotfiles-bak/${BACKUPTIME}"
+
+    if [ ! -d ${BACKUPDIR} ]
+    then
+        mkdir -p ${BACKUPDIR}
+    fi
+
+    if [ -f ${SYMLINK} ]
+    then
+        mv -f ${SYMLINK} ${BACKUPDIR}
+    fi
+
+    echo "${PWD}/${DOTFILE} => ${SYMLINK}"
+    ln -fs ${PWD}/${DOTFILE} ${SYMLINK}
 done
+
+vim +BundleInstall +qa\!
