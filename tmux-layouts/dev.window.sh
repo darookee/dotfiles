5157e7393979382c6d51ee18659d9d64c918e66b
darookee <nils.uliczka@darookee.net>
2013-09-25 17:08:02 +0200

    [misc] removed tmuxifier, tree, yuicompressor

--- tmux-layouts/dev.window.sh
+++
@@ -1,9 +0,0 @@
-window_root "~/Dev"
-
-new_window "dev"
-
-run_cmd vi +CtrlP
-split_v 25
-split_h 50
-
-select_pane 0
--- tmux-layouts/devKunden.window.sh
+++
@@ -1,9 +0,0 @@
-window_root "~/Dev/Kunden/${1}"
-
-new_window "${1}"
-
-run_cmd vi
-split_v 25
-split_h 50
-
-select_pane 0
