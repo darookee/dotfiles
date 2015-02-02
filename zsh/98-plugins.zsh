ed409c01c9c7404737c9ad167217d2f68ef5c098
Nils Uliczka <nils.uliczka@darookee.net>
2015-02-03 00:45:19 +0100

    [zsh] syntaxhighlight - sudo lines in yellow

--- zsh/98-plugins.zsh
+++ zsh/98-plugins.zsh
@@ -18,7 +18,7 @@ bindkey -M vicmd "j" history-substring-search-down
 #
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 ZSH_HIGHLIGHT_PATTERNS+=(' -rf ' 'fg=white,bold,bg=red')
-ZSH_HIGHLIGHT_PATTERNS+=('sudo ' 'fg=yellow,bold')
+ZSH_HIGHLIGHT_PATTERNS+=('sudo *' 'fg=yellow,bold')
 ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red,bold'
 ZSH_HIGHLIGHT_STYLES[alias]='fg=cyan'
 # }}}
