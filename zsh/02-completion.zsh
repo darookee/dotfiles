f24808cbe089fdd7a42c91459b6cb25bb607d7cc
Nils Uliczka <nils.uliczka@darookee.net>
2015-02-03 01:26:10 +0100

    [zsh] add propper symfony2 app/console completion

--- zsh/02-completion.zsh
+++ zsh/02-completion.zsh
@@ -79,10 +79,11 @@ _symfony_console () {
   echo "php $(find . -maxdepth 2 -mindepth 1 -name 'console' -type f | head -n 1)"
 }
 _symfony2_get_command_list () {
-   `_symfony_console` --no-ansi | sed "1,/Available commands/d" | awk '/^  ?[a-z]+/ { print $1 }'
+    `_symfony_console` --no-ansi|sed "1,/Available commands/d"|grep :|(while read -r line; do echo $line; done;)|awk '{ gsub(/:/,"\\:",$1); sub(/[  ]/,":", $0); gsub(/[  ]/,"\\ ", $0); print $0; }'
 }
 _symfony2 () {
-   compadd `_symfony2_get_command_list`
+   sf2commands=("${(@f)$(_symfony2_get_command_list)}")
+   _describe -t sf2commands 'Available commands' sf2commands
 }
 compdef _symfony2 '`_symfony_console`'
 compdef _symfony2 'app/console'
