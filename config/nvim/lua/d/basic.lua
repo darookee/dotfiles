3f3ea1ed9ad3e2c15548fd336ca389035679e155
Nils Uliczka <nils.uliczka@darookee.net>
2023-09-11 10:36:27 +0200

    [vim] fix guicursor

--- config/nvim/lua/d/basic.lua
+++ config/nvim/lua/d/basic.lua
@@ -13,6 +13,7 @@ local setup_options = function()
     wo.relativenumber = true
     opt.termguicolors = true
     opt.cursorline = true
+    opt.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
 
     if fn.has 'persistent_undo' then
         local target_path = fn.expand('~/.local/share/nvim/undo')
